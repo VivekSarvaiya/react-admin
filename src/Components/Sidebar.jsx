ea967f697b789bed86016a7b72d3dbaacd890cf0
VivekSarvaiya <viveksarvaiya.dds@gmail.com>
2023-04-12 19:16:04 +0530

    header setting

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -31,7 +31,7 @@
 // import axios from "axios";
 // import Swal from "sweetalert2";
 // import { Badge } from "antd";
-// import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+// import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 // const AppBar = styled(MuiAppBar, {
 //   shouldForwardProp: (prop) => prop !== "open",
 // })(({ theme, open }) => ({
@@ -308,14 +308,8 @@
 // }
 
 // export default Sidebar;
-import React, { useState } from "react";
-import {
-  MenuFoldOutlined,
-  MenuUnfoldOutlined,
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-} from "@ant-design/icons";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../Context/userContext";
 import {
   AccountCircle,
   Engineering,
@@ -326,17 +320,87 @@ import {
   Rule,
   Settings,
 } from "@mui/icons-material";
-import { Layout, Menu, theme } from "antd";
+import FormatIndentIncreaseRoundedIcon from "@mui/icons-material/FormatIndentIncreaseRounded";
+import FormatIndentDecreaseRoundedIcon from "@mui/icons-material/FormatIndentDecreaseRounded";
+import { Badge, Layout, Menu, theme } from "antd";
 import { Outlet, useNavigate } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import { Dropdown, Avatar } from "antd";
+import Swal from "sweetalert2";
+import axios from "axios";
+
 const { Header, Sider, Content } = Layout;
+
+const profileImg = "";
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const { authState } = useContext(AuthContext);
   const {
     token: { colorBgContainer },
   } = theme.useToken();
   const nav = useNavigate();
 
+  const handleLogout = () => {
+    Swal.fire({
+      title: `Are you sure , you want to Logout`,
+      icon: "question",
+      showDenyButton: true,
+      confirmButtonText: "Yes",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .post(
+            ` ${process.env.REACT_APP_BASE_URL}/api/usersLogOut/`,
+            {},
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("TOKEN")}`,
+              },
+            }
+          )
+          .then((res) => {
+            console.log(res);
+            localStorage.clear();
+            nav("/login");
+          })
+          .catch((err) => {
+            console.log(err);
+            Swal.fire(`Something went wrong !`, "", "error");
+          });
+      }
+    });
+  };
+  const profileMenu = (
+    <div className="nav-profile nav-dropdown">
+      <div className="nav-profile-header">
+        <div className="d-flex">
+          <Avatar size={45} src={profileImg} />
+          <div className="pl-3">
+            <h4 className="mb-0"> {authState?.username}</h4>
+            <span className="text-muted">{authState?.email}</span>
+          </div>
+        </div>
+      </div>
+      <div className="nav-profile-body">
+        <Menu>
+          <Menu.Item onClick={() => nav("/profile")}>
+            <span>
+              <AccountCircle color="action" className="mr-5" />
+              <span className="font-weight-normal"> Profile</span>
+            </span>
+          </Menu.Item>
+          <Menu.Item onClick={() => nav("/changepassword")}>
+            <span>
+              <Settings color="action" className="mr-5" />
+              <span className="font-weight-normal"> Change Password</span>
+            </span>
+          </Menu.Item>
+        </Menu>
+      </div>
+    </div>
+  );
   return (
     <Layout>
       <Sider theme="" trigger={null} collapsible collapsed={collapsed}>
@@ -388,14 +452,55 @@ const Sidebar = () => {
         />
       </Sider>
       <Layout className="site-layout">
-        <Header style={{ padding: "0 10px", background: colorBgContainer }}>
-          {React.createElement(
-            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-            {
-              className: "trigger",
-              onClick: () => setCollapsed(!collapsed),
-            }
-          )}
+        <Header
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            padding: "0 10px",
+            background: colorBgContainer,
+          }}
+        >
+          <div style={{ cursor: "pointer" }}>
+            {collapsed ? (
+              <FormatIndentIncreaseRoundedIcon
+                color="action"
+                onClick={() => setCollapsed(!collapsed)}
+              />
+            ) : (
+              <FormatIndentDecreaseRoundedIcon
+                color="action"
+                onClick={() => setCollapsed(!collapsed)}
+              />
+            )}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <IconButton>
+              <Badge count={5}>
+                <NotificationsNoneOutlinedIcon color="action" />
+              </Badge>
+            </IconButton>
+            <Dropdown
+              placement="bottomRight"
+              overlay={profileMenu}
+              trigger={["click"]}
+            >
+              <Menu className="d-flex align-item-center" mode="horizontal">
+                <Menu.Item>
+                  <Avatar src={profileImg} className="w-[25px] h-[25px]" />
+                </Menu.Item>
+              </Menu>
+            </Dropdown>
+            <IconButton onClick={handleLogout}>
+              <Logout color="action" />
+            </IconButton>
+          </div>
         </Header>
         <Content
           style={{
--- src/Views/Dashboard.jsx
+++ src/Views/Dashboard.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../Context/userContext";
 import LineChart from "../Charts/LineChart";
 import Donut from "../Charts/Donut";
+import ReactApexChart from "react-apexcharts";
 
 function Dashboard(props) {
   const [open, setOpen] = useState(true);
@@ -18,6 +19,46 @@ function Dashboard(props) {
   useEffect(() => {
     console.log(authState);
   }, []);
+
+  const series = [
+    {
+      name: "series1",
+      data: [31, 40, 28, 51, 42, 109, 100],
+    },
+    {
+      name: "series2",
+      data: [11, 32, 45, 32, 34, 52, 41],
+    },
+  ];
+  const options = {
+    chart: {
+      height: 350,
+      type: "area",
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      type: "datetime",
+      categories: [
+        "2018-09-19T00:00:00.000Z",
+        "2018-09-19T01:30:00.000Z",
+        "2018-09-19T02:30:00.000Z",
+        "2018-09-19T03:30:00.000Z",
+        "2018-09-19T04:30:00.000Z",
+        "2018-09-19T05:30:00.000Z",
+        "2018-09-19T06:30:00.000Z",
+      ],
+    },
+    tooltip: {
+      x: {
+        format: "dd/MM/yy HH:mm",
+      },
+    },
+  };
 
   return (
     <>
@@ -148,7 +189,13 @@ function Dashboard(props) {
         </div>
       </div>
       <div className="p-4">
-        {/* <h2 className="text-center">Issues Posted Data</h2> */}
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="area"
+          height={350}
+        />
+
         <div className="d-flex">
           <LineChart />
           <Donut />
